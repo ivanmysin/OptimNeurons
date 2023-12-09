1bfa963fc37e980a5a6dd95ebff9e8f3b82d532f
ivan <imysin@mail.ru>
2023-12-09 14:55:03 +0300

    Model works. Need test

--- main.py
+++ main.py
@@ -3,11 +3,25 @@ import slib as lib
 import net_params as pr
 import matplotlib.pyplot as plt
 def main():
-    neurons_params = [pr.theta_generators, pr.neuron_params]
+    Duration = 5000 # Время симуляции в ms
+    Distance = Duration * 0.001 * pr.V_AN  # Расстояние, которое пробегает животное за время симуляции в cm
+
+
+
+    neurons_params = [pr.theta_generators, pr.theta_spatial_generators_soma, pr.theta_spatial_generators_dend, pr.neuron_params]
     neurons_idx_by_names = {}
 
     for neuron_idx, neuron in enumerate(neurons_params):
         neuron["class"] = getattr(lib, neuron["class"])
+
+        try:
+            for neuron_param in neuron["params"]:
+                neuron_param["sp_centers"] += 0.5*Distance
+        except KeyError:
+            pass
+
+
+
         neurons_idx_by_names[neuron["name"]] = neuron_idx
 
     synapses_params = []
@@ -30,14 +44,14 @@ def main():
         synapses_params.append(synapse_param)
 
     net = lib.Network(neurons_params, synapses_params)
-    net.integrate(0.1, 1000)
+    net.integrate(0.1, Duration)
 
-    Vsoma = net.get_neuron_by_idx(1).getCompartmentByName('soma').getVhist()
-    Vdend = net.get_neuron_by_idx(1).getCompartmentByName('dendrite').getVhist()
+    Vsoma = net.get_neuron_by_idx(-1).getCompartmentByName('soma').getVhist()
+    Vdend = net.get_neuron_by_idx(-1).getCompartmentByName('dendrite').getVhist()
 
-    firing = net.get_neuron_by_idx(1).getCompartmentByName('soma').getFiring() / 0.0001
+    firing = net.get_neuron_by_idx(-1).getCompartmentByName('soma').getFiring() / 0.0001
 
-    t = np.linspace(0, 1.0, Vsoma.size)
+    t = np.linspace(0, 0.001*Duration, Vsoma.size)
 
     fig,  axes = plt.subplots(nrows=2, sharex=True)
     axes[0].plot(t, Vsoma)
