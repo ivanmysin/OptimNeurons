16e65ed257dd306a662b48bb599de9383599ac2c
ivan <imysin@mail.ru>
2024-04-20 11:27:56 +0300

    Absolute normalization is nice

--- optim_syns.py
+++ optim_syns.py
@@ -161,7 +161,7 @@ def simulate(X, Duration, dt, Cm, animal_velocity, params_generators, params_syn
 
     Erev_hist = np.sum(g_hist*Erev, axis=1) / (G_tot + 0.00000001)
 
-    tau_m_hist = G_tot / Cm
+    tau_m_hist = Cm / (G_tot + 0.1)
 
     return Erev_hist, tau_m_hist
 ###############################################################
@@ -312,7 +312,7 @@ timer = time.time()
 
 print('starting optimization ... ')
 sol = differential_evolution(Loss, x0=X, popsize=32, atol=1e-3, recombination=0.7, \
-                                 mutation=[0.2, 1.5], bounds=bounds, maxiter=500, \
+                                 mutation=[0.2, 0.8], bounds=bounds, maxiter=500, \
                                  workers=-1, updating='deferred', disp=True, strategy='best2bin', \
                                  polish=True, args=args, callback=callback)
 callback(sol)
